ae411beb8064eed045cda994702e14b7e2e5562c
Manoj-M19 <manojpavithra2004@gmail.com>
2026-01-29 11:56:43 +0000

    Created workspace route

---
+++ app/api/workspaces/route.ts
@@ -0,0 +1,91 @@
+import { db } from "@/lib/db";
+import { i } from "framer-motion/client";
+import { Postpone } from "next/dist/server/app-render/dynamic-rendering";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "userId is required" },
+        { status: 400 },
+      );
+    }
+
+    const workspaces = await db.workspace.findMany({
+      where: { userId },
+      orderBy: { createdAt: "desc" },
+    });
+    return NextResponse.json({ workspaces });
+  } catch (error) {
+    console.error("Error fetching workspaces:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch workspaces" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const { name, userId } = body;
+
+    if (!name || !userId) {
+      return NextResponse.json(
+        { error: "name and userId are required" },
+        { status: 400 },
+      );
+    }
+
+    const slug = name
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/(^-|-$)/g, "");
+
+    const workspace = await db.workspace.create({
+      data: {
+        name,
+        slug: `${slug}-${Date.now()}`,
+        userId,
+      },
+    });
+
+    return NextResponse.json({ workspace }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating workspace:", error);
+    return NextResponse.json(
+      { error: "Failed to create workspace" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(req:NextRequest) {
+    try {
+        const {searchParams} = new URL(req.url);
+        const id = searchParams.get("id");
+
+        if(!id) {
+            return NextResponse.json(
+                {error:"Wrokspace id is required"},
+                {status:400}
+            );
+        }
+
+        await db.workspace.delete({
+            where:{id},
+        });
+
+        return NextResponse.json({success:true});
+    } catch (error) {
+        console.error("Error deleting workspace",error);
+        return NextResponse.json(
+            {error:"Failed to delete workspace"},
+            {status:500}
+        );
+    }
+}
